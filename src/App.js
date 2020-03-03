414b1906b67336873b2ed4696121334c7d31db61
Jirka-Lhotka <jl887@alumni.cam.ac.uk>
2020-03-03 12:08:10 +0100

    Komit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Kokso lokso</h1>
+      dadsadasdas
       <h2>Start editing to see some magic happen!</h2>
     </div>wfeioewfhioewhfoiewefhiwfewhoifewh
   );
